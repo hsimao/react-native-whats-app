a24aa3c53f25ed70f0a620c37d0c59e4696f7e71
hsimao <e087754958@gmail.com>
2022-10-22 17:15:03 +0800

    feat: add safe area

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   const handleClick = () => {
     console.log('click')
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text style={styles.label}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
 
-      <Button title="Click me" onPress={handleClick} />
-    </View>
+        <Button title="Click me" onPress={handleClick} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
