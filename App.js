82c47af6bc0165d4549a78183260295c42488c07
hsimao <e087754958@gmail.com>
2022-10-23 00:34:13 +0800

    feat: 啟動畫面

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect, useState, useCallback } from 'react'
+
+// 維持啟動畫面
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  const handleClick = () => {
-    console.log('click')
-  }
+  const [appIsLoaded, setAppIsLoaded] = useState(false)
+
+  // load fonts
+  useEffect(() => {
+    setTimeout(() => {
+      console.warn('iohe hello')
+      setAppIsLoaded(true)
+    }, 3000)
+  })
+
+  // 關閉啟動畫面
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) await SplashScreen.hideAsync()
+  }, [appIsLoaded])
+
+  // 未完成加載前不渲染任何樣式
+  if (!appIsLoaded) return null
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
       <SafeAreaView>
         <Text style={styles.label}>
           Open up App.js to start working on your app!
         </Text>
         <StatusBar style="auto" />
-
-        <Button title="Click me" onPress={handleClick} />
       </SafeAreaView>
     </SafeAreaProvider>
   )
