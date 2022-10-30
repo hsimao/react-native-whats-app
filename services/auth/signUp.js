86e20bc990bbe4f0697962035a73fc6640ba4576
hsimao <e087754958@gmail.com>
2022-10-30 13:04:10 +0800

    feat: add async-storage, save user data

--- services/auth/signUp.js
+++ services/auth/signUp.js
@@ -1,18 +1,30 @@
 import { auth } from '../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { createUser } from '../users/createUser'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const signUp = async ({ firstName, lastName, email, password }) => {
   try {
+    // create auth user
     const result = await createUserWithEmailAndPassword(auth, email, password)
     const { uid, stsTokenManager } = result.user
-    const { accessToken } = stsTokenManager
+    const { accessToken, expirationTime } = stsTokenManager
 
+    const formatExpiryDate = new Date(expirationTime)
+
+    // save user to database
     const userData = await createUser({
       firstName,
       lastName,
       email,
       userId: uid,
+    })
+
+    // save user data 到用戶裝置
+    saveDataToStorage({
+      token: accessToken,
+      userId: uid,
+      expiryDate: formatExpiryDate,
     })
 
     return { ...userData, token: accessToken }
@@ -26,4 +38,16 @@ export const signUp = async ({ firstName, lastName, email, password }) => {
 
     throw new Error(message)
   }
+}
+
+// 儲存 userData 到用戶裝置
+const saveDataToStorage = ({ token, userId, expiryDate }) => {
+  AsyncStorage.setItem(
+    'userData',
+    JSON.stringify({
+      token,
+      userId,
+      expiryDate: expiryDate.toISOString(),
+    })
+  )
 }
