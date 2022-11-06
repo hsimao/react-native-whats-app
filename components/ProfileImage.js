f4bcf867aba301a47d9704636b77ad9d55752aea
hsimao <e087754958@gmail.com>
2022-11-06 16:43:14 +0800

    feat: Profile image add loading

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -6,14 +6,30 @@ import { launchImagePicker } from '../utils/imagePicker'
 import userImage from '../assets/images/userImage.jpeg'
 import { uploadAvatar } from '../services/upload/uploadAvatar'
 import { updateUser } from '../services/users/updateUser'
+import { colors } from '../theme/colors'
+
+const ImageContainer = styled.View`
+  width: ${props => (props.size ? `${props.size}px` : '80px')};
+  height: ${props => (props.size ? `${props.size}px` : '80px')};
+  justify-content: center;
+  align-items: center;
+`
+const Loading = styled.ActivityIndicator.attrs({
+  color: colors.primary,
+  size: 'small',
+})`
+  z-index: 1000;
+  position: absolute;
+`
 
 const Image = styled.Image.attrs({
   resizeMode: 'contain',
 })`
+  opacity: ${props => (props.isLoading ? '0' : '1')};
   border: solid 1px ${({ theme }) => theme.colors.grey};
   border-radius: ${props => (props.size ? `${props.size / 2}px` : '40px')};
-  width: ${props => (props.size ? `${props.size}px` : '80px')};
-  height: ${props => (props.size ? `${props.size}px` : '80px')};
+  width: 100%;
+  height: 100%;
 `
 
 const EditIcon = styled.View`
@@ -28,14 +44,20 @@ const EditIcon = styled.View`
 const ProfileImage = props => {
   const { userId, updateUserData, uri, size } = props
 
+  const [isLoading, setIsLoading] = useState(false)
+
   // default image
   const source = uri ? { uri } : userImage
   const [image, setImage] = useState(source)
 
   const pickImage = useCallback(async () => {
+    if (isLoading) return
+
     try {
       const tempUri = await launchImagePicker()
       if (!tempUri) return
+
+      setIsLoading(true)
 
       // upload image to firebase storage
       const uploadUrl = await uploadAvatar(userId, tempUri)
@@ -51,13 +73,28 @@ const ProfileImage = props => {
 
       setImage({ uri: uploadUrl })
     } catch (error) {
+      setIsLoading(false)
       console.log(error)
     }
   }, [userId, updateUserData])
 
   return (
     <TouchableOpacity onPress={pickImage}>
-      <Image source={image} size={size} />
+      <ImageContainer size={size}>
+        <Image
+          source={image}
+          isLoading={isLoading}
+          size={size}
+          onLoadEnd={() => setIsLoading(false)}
+        />
+        {isLoading ? (
+          <Loading
+            style={{ zIndex: 1000 }}
+            size={'small'}
+            color={colors.primary}
+          />
+        ) : null}
+      </ImageContainer>
 
       <EditIcon>
         <FontAwesome name="pencil" size={15} color="black" />
