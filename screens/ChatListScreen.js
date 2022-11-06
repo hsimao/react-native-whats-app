71e894fa086cc8ff476a147f2ca6dfd5a43aca56
hsimao <e087754958@gmail.com>
2022-11-06 17:38:14 +0800

    feat: add header buttons

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import CustomHeaderButton from '../components/CustomHeaderButtons'
 
 const ChatListScreen = ({ navigation }) => {
   const handleToSetting = () => {
     navigation.navigate('ChatScreen')
   }
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item
+              title="New chat"
+              iconName="create-outline"
+              onPress={() => {}}
+            />
+          </HeaderButtons>
+        )
+      },
+    })
+  }, [])
 
   return (
     <View style={styles.container}>
