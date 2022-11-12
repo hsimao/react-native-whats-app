4bbd5cd13b24631ea55e3c83353c675f3eb2a777
hsimao <e087754958@gmail.com>
2022-11-12 17:46:51 +0800

    feat: message slice, on message

--- store/hooks.js
+++ store/hooks.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux'
 import { authSlice } from './authSlice'
 import { userSlice } from './userSlice'
 import { chatSlice } from './chatSlice'
+import { messageSlice } from './messageSlice'
 
 // 封裝 dispatch, 直接調用取出各別 slice actions 方法即可, 不用在各別引入組合
 /**
@@ -13,7 +14,12 @@ import { chatSlice } from './chatSlice'
 export const useActions = () => {
   const dispatch = useDispatch()
   return bindActionCreators(
-    { ...authSlice.actions, ...userSlice.actions, ...chatSlice.actions },
+    {
+      ...authSlice.actions,
+      ...userSlice.actions,
+      ...chatSlice.actions,
+      ...messageSlice.actions,
+    },
     dispatch
   )
 }
--- store/store.js
+++ store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
 import userSlice from './userSlice'
 import chatSlice from './chatSlice'
+import messageSlice from './messageSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     user: userSlice,
     chat: chatSlice,
+    message: messageSlice,
   },
 })
